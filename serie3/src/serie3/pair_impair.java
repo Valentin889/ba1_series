114e08274bf179edafacfd906ca7fd037f10f100
Valentin889 <thebiggbosse889@gmail.com>
2021-10-08 09:01:56 +0200

    Add pair_impair serie3

---
+++ serie3/src/serie3/pair_impair.java
@@ -0,0 +1,24 @@
+package serie3;
+import java.util.Scanner;
+
+
+public class pair_impair {
+	public static void main (String args[]) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		System.out.print("Entrer un nombre entier : ");
+		int valeur = scanner.nextInt();
+		
+		if(valeur == 0){
+			System.out.print("Le nombre est zéro (et il est pair)");	
+		} else if(valeur % 2 == 0) {
+			System.out.print("Le nombre est pair");	
+		} else {
+			System.out.print("Le nombre est impair");		
+		}
+		
+		
+	}
+}
