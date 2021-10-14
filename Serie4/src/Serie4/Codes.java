edce8de2f17d988fd4a3f75e03c0a724ffa50024
Valentin889 <thebiggbosse889@gmail.com>
2021-10-14 15:50:49 +0200

    Add Codes.java

---
+++ Serie4/src/Serie4/Codes.java
@@ -0,0 +1,23 @@
+package Serie4;
+
+public class Codes {
+	public static void main(String[] args) {
+		
+		char val1 = '*';
+		char val2 = '9';
+		char val3 = 'w';
+
+		char val4 = (char)34;
+		char val5 = (char)65;
+		char val6 = (char)123;
+		
+		
+		System.out.println((int)val1);
+		System.out.println((int)val2);
+		System.out.println((int)val3);
+		System.out.println(val4);
+		System.out.println(val5);
+		System.out.println(val6);
+		
+	}
+}
