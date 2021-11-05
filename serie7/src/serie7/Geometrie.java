bee55a3d66101ca82241eb727c3f8c1e961bffde
Valentin889 <thebiggbosse889@gmail.com>
2021-11-05 14:41:51 +0100

    Add exo Geometrie

---
+++ serie7/src/serie7/Geometrie.java
@@ -0,0 +1,11 @@
+package serie7;
+
+public class Geometrie {
+	public static void main(String[] args) {
+		
+		Triange t1 = new Triange(new Point(1,1), new Point(2,2), new Point(3,3));
+		
+		t1.afficherPerimetre();
+		
+	}
+}
---
+++ serie7/src/serie7/Triange.java
@@ -0,0 +1,43 @@
+package serie7;
+
+import java.util.Iterator;
+
+public class Triange {
+	private Point[] points;
+	
+	public Triange(Point p1, Point p2, Point p3) {
+		points = new Point[] {p1,p2,p3};
+		
+	}
+	
+	
+	public void afficherPerimetre() {
+		System.out.println("le perimetre du triange est : "+calculerPerimetre());
+	}
+	
+	public boolean isIsocele() {
+		
+		return false;
+	}
+	
+	
+	
+	private double calculerPerimetre() {
+		
+		double perimetre = 0;
+		
+		
+		for (int i = 0; i < points.length; i++) {
+			Point p1 = points[i];
+			Point p2 = i+1==points.length ? points[0] : points[i+1];
+			double dist = Math.sqrt(Math.pow(p1.getX() - p2.getX(),2) + Math.pow(p1.getY() - p2.getY(),2));
+			perimetre+=dist;
+			
+		}
+		
+		return perimetre;
+		
+	}
+	
+	
+}
